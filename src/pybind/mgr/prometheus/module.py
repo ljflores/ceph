scantist-ossops-m2/ceph-ci

mgr/prometheus: use vendored "packaging" instead
backport of: cf6089200d96fc56b08ee17a4e31f19823370dc8

Signed-off-by: Matan Breizman <mbreizma@redhat.com>

src/pybind/mgr/prometheus/module.py
@@ -1,6 +1,6 @@
 import cherrypy
 from collections import defaultdict
-from packaging.version import Version
+from pkg_resources import packaging # type: ignore
 import json
 import math
 import os
@@ -32,6 +32,7 @@ DEFAULT_PORT = 9283
 # ipv6 isn't yet configured / supported and CherryPy throws an uncaught
 # exception.
 if cherrypy is not None:
+    Version = packaging.version.Version
     v = Version(cherrypy.__version__)
     # the issue was fixed in 3.2.3. it's present in 3.2.2 (current version on
     # centos:7) and back to at least 3.0.0.

ljflores/ceph

(cherry picked from commit 0e4ead7bb8fa21f9e79ae56c07c992efee3492cb)
Conflicts:
- ceph.spec.in: extra "%endif" in octopus that was removed in pacific
- debian/control: octopus uses a different format to list packages than pacific
- src/pybind/mgr/prometheus/module.py: extra "distutils" line in octopus that was removed in pacific; octopus uses StrictVersion instead of Version
- src/pybind/mgr/requirements.txt: extra space in octopus that was removed in pacific

src/pybind/mgr/prometheus/module.py
@@ -1,6 +1,7 @@
 import cherrypy
 from collections import defaultdict
 from distutils.version import StrictVersion
+from pkg_resources import packaging  # type: ignore
 import json
 import errno
 import math
@@ -33,6 +34,7 @@ DEFAULT_PORT = 9283
 # ipv6 isn't yet configured / supported and CherryPy throws an uncaught
 # exception.
 if cherrypy is not None:
+    StrictVersion = packaging.version.StrictVersion
     v = StrictVersion(cherrypy.__version__)
     # the issue was fixed in 3.2.3. it's present in 3.2.2 (current version on
     # centos:7) and back to at least 3.0.0.