ac36227f10d4dcc7ee33d2eb710483165ec26d9a
Laura Flores <lflores@ibm.com>
2024-05-22 18:03:04 -0500

    modify p2p test to reproduce pg-upmap-primary encoding upgrade bug
    Related to: https://tracker.ceph.com/issues/61948
    Signed-off-by: Laura Flores <lflores@ibm.com>

---
+++ qa/workunits/test_primary_mapping.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pgid=$(ceph pg dump pgs_brief -f json | jq '.pg_stats[0]' | jq -r '.pgid')
+new_prim=$(ceph pg dump pgs_brief -f json | jq '.pg_stats[0]' | jq -r '.up[-1]')
+
+ceph osd set-require-min-compat-client reef
+ceph osd pg-upmap-primary "${pgid}" "${new_prim}"
