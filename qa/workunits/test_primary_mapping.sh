41a5b6132eb3e26d00ebe92ac230afa83a5fd4f5
Laura Flores <lflores@ibm.com>
2024-05-23 11:25:04 -0500

    change min compat client to squid
    Signed-off-by: Laura Flores <lflores@ibm.com>

--- qa/workunits/test_primary_mapping.sh
+++ qa/workunits/test_primary_mapping.sh
@@ -3,5 +3,5 @@
 pgid=$(ceph pg dump pgs_brief -f json | jq '.pg_stats[0]' | jq -r '.pgid')
 new_prim=$(ceph pg dump pgs_brief -f json | jq '.pg_stats[0]' | jq -r '.up[-1]')
 
-ceph osd set-require-min-compat-client reef
+ceph osd set-require-min-compat-client squid
 ceph osd pg-upmap-primary "${pgid}" "${new_prim}"
