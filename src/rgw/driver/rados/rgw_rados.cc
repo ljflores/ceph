e4d51435725a5e9199d9c43bc264705aa11ef19d
Casey Bodley <cbodley@redhat.com>
2025-12-02 16:19:05 -0500

    rgw/rados: fix init/shutdown order for RGWIndexCompletionManager
    RGWRados::init_complete() initializes this RGWIndexCompletionManager
    after starting some background threads that depend on it, like RGWLC and
    RGWDataSyncProcessorThread and RGWObjectExpirer. this can lead to a
    crash when accessing a null index_completion_manager
    
    RGWRados::finalize() deletes it before stopping the Restore thread
    
    Fixes: https://tracker.ceph.com/issues/74067
    
    Reported-by: J. Eric Ivancich <ivancich@redhat.com>
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 06188d0b2057e3242e2f69824bf52f23ccf868fa)
    Resolves: IBMCEPH-14805
    
    Conflicts:
    	src/rgw/driver/rados/rgw_rados.cc  run_bucket_logging_thread not on 8.1

--- src/rgw/driver/rados/rgw_rados.cc
+++ src/rgw/driver/rados/rgw_rados.cc
@@ -1101,7 +1101,6 @@ void RGWRados::finalize()
     reshard->stop_processor();
   }
   delete reshard;
-  delete index_completion_manager;
 
   if (run_notification_thread) {
     rgw::notify::shutdown();
@@ -1113,6 +1112,7 @@ void RGWRados::finalize()
   }
   delete restore;
   restore = NULL;
+  delete index_completion_manager;
 }
 
 /** 
@@ -1229,6 +1229,8 @@ int RGWRados::init_complete(const DoutPrefixProvider *dpp, optional_yield y)
     return ret;
 
   pools_initialized = true;
+
+  index_completion_manager = new RGWIndexCompletionManager(this);
 
   if (use_gc) {
     gc = new RGWGC();
@@ -1377,8 +1379,6 @@ int RGWRados::init_complete(const DoutPrefixProvider *dpp, optional_yield y)
   if (run_reshard_thread)  {
     reshard->start_processor();
   }
-
-  index_completion_manager = new RGWIndexCompletionManager(this);
 
   if (run_notification_thread) {
     if (!rgw::notify::init(dpp, driver, *svc.site)) {
