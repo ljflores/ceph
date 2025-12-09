120d52586b6bfffbef72ee0d64f762fa1384ee94
Soumya Koduri <skoduri@redhat.com>
2025-12-09 22:15:27 +0530

    rgw/lc: fix potential use_after_free during transition
    There is a reference of transition rule action used by work pool
    threads which may go out of context if the threads are still active
    while the LC processor is being stopped. This change fixes it by doing
    deep copy of the variable.
    
    Resolves: IBMCEPH-17524
    
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>
    (cherry picked from commit bcb2ec8cca0522203304fc3f0899a999097404cb)

--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -1398,7 +1398,7 @@ public:
 };
 
 class LCOpAction_Transition : public LCOpAction {
-  const transition_action& transition;
+  transition_action transition;
   bool need_to_process{false};
 
 protected:
